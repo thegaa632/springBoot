b8f0b784cad338fe06162a762c88cec0714e5121
thegaa632 <thegaa632@gmail.com>
2024-05-13 19:10:13 +0900

    깃 연동

---
+++ springBootST/src/main/java/com/boot/s1/SpringBootStApplication.java
@@ -0,0 +1,13 @@
+package com.boot.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStApplication.class, args);
+	}
+
+}
---
+++ springBootST/src/test/java/com/boot/s1/SpringBootStApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
