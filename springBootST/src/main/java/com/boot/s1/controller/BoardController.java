5b75ec1103710c26ca6094d3f67ed1934438f39d
thegaa632 <thegaa632@gmail.com>
2024-06-04 23:16:11 +0900

    게시글 읽기, 수정, 삭제 파트 활성화 및 버튼 연동

--- springBootST/src/main/java/com/boot/s1/controller/BoardController.java
+++ springBootST/src/main/java/com/boot/s1/controller/BoardController.java
@@ -1,17 +1,22 @@
 package com.boot.s1.controller;
 
+import com.boot.s1.domain.Board;
 import com.boot.s1.dto.PageRequestDTO;
 import com.boot.s1.dto.PageResponseDTO;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.boot.s1.service.BoardService;
 import com.boot.s1.dto.BoardDTO;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -29,5 +34,81 @@ public class BoardController {
         log.info("List : responseDTO :" + responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+//    등록 파트
+//    화면단 표시
+    @GetMapping("/register")
+    public void registerGET(BoardDTO boardDTO) {
+
+    }
+//    실 데이터 처리
+    @PostMapping("/register")
+    public String registerPOST(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("board POST register");
+//      에러 발생시 모든 에러를 redirectAttributes에 추가로 전송함
+        if(bindingResult.hasErrors()) {
+            log.info("has error...");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+//    조회/수정 파트
+    @GetMapping({"/read", "/modify"})
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("board modify post ... " + boardDTO);
+
+        if (bindingResult.hasErrors()) {
+            log.info("has error...");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify?" + link;
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read";
+    }
+
+//    삭제 파트
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+
+        log.info("remove post ... " + bno);
+
+        boardService.remove(bno);
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/list";
     }
 }
--- springBootST/src/main/java/com/boot/s1/dto/BoardDTO.java
+++ springBootST/src/main/java/com/boot/s1/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package com.boot.s1.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Builder;
@@ -13,9 +15,18 @@ import java.time.LocalDateTime;
 public class BoardDTO {
 
     private long bno;
+
+//  등록시 비어 있지 않도록 처리
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String writer;
+
+    @NotEmpty
     private String content;
+
     private LocalDateTime regDate;
     private LocalDateTime modDate;
 
