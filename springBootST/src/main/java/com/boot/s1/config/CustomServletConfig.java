19bc2df85663069956b1f511d1a4e6fec0978186
thegaa632 <thegaa632@gmail.com>
2024-06-08 23:12:02 +0900

    swagger 연동

---
+++ springBootST/src/main/java/com/boot/s1/config/CustomServletConfig.java
@@ -0,0 +1,11 @@
+package com.boot.s1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+//swagger 접속을 위한 별도의 환경 설정
+public class CustomServletConfig {
+
+}
---
+++ springBootST/src/main/java/com/boot/s1/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.boot.s1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.boot.s1.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //api의 제목
+                .title("Test api")
+                //설명
+                .description("SpringBoot Project Swagger")
+                //버전
+                .version("1.0.0")
+                .build();
+
+    }
+}
