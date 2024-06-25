1825141d6972029bfc87316f18934326caafa0ba
thegaa632 <thegaa632@gmail.com>
2024-06-25 16:01:45 +0900

    modal 오류 수정

--- springBootST/src/main/resources/static/js/reply.js
+++ springBootST/src/main/resources/static/js/reply.js
@@ -1,7 +1,16 @@
 async function getList({bno, page, size, goList}) {
     const result = await axios.get(`/replies/list/${bno}`, {params : {page, size}})
-    // console.log(`bno : ${bno}`);
-    // console.log("result : " + JSON.stringify(result.data));
+
+    if(goList) {
+        const total = result.data.total;
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
 
     return result.data;
+}
+
+async function addReply(replyObj) {
+    const response = await axios.post(`/replies/`, replyObj)
+    return response.data;
 }
