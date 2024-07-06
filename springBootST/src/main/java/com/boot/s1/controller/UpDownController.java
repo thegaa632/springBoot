90fe53a86e69889a4f80bc2ddd3a88d594834591
thegaa632 <thegaa632@gmail.com>
2024-07-06 16:53:57 +0900

    업로드 추가중(swagger)//버전 바꾸는 중

--- springBootST/src/main/java/com/boot/s1/controller/UpDownController.java
+++ springBootST/src/main/java/com/boot/s1/controller/UpDownController.java
@@ -2,15 +2,12 @@ package com.boot.s1.controller;
 
 import com.boot.s1.dto.upload.UpLoadFileDTO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -21,34 +18,40 @@ import java.util.UUID;
 @Log4j2
 public class UpDownController {
 
-    @Value("${com.boot.s1.upload.path}")
-    private String upLoadPath;
-//    @Operation(summary = "UpLoad POST", description = "POST 방식으로 파일 등록함")
-    @Operation(summary = "UpLoad POST", description = "POST 방식으로 파일 등록함", requestBody = @RequestBody(content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "object", format = "binary"))))
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(@RequestPart("files")UpLoadFileDTO upLoadFileDTO) {
+//    @Value("${com.boot.s1.upload.path}")
+//    private String upLoadPath;
 
-        log.info(upLoadFileDTO);
+    @Operation(summary = "UpLoad POST", description = "POST 방식으로 파일 등록함")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = "application/json", consumes = "multipart/form-data")
+    public ResponseEntity<UpLoadFileDTO> upload(@Parameter(name = "fileName", description = "업로드 파일", in = ParameterIn.QUERY)
+                                                @RequestParam(value = "fileName")
+                                                    String fileName,
+                                                @Parameter(name = "files", description = "업로드 파일")
+                                                @RequestParam(value = "files")
+                                                UpLoadFileDTO files) {
 
-        if(upLoadFileDTO.getFiles() != null) {
+        UpLoadFileDTO result = UpLoadFileDTO.builder().fileName(fileName).build();
 
-            upLoadFileDTO.getFiles().forEach(multipartFile -> {
+        return new ResponseEntity<>(result, HttpStatus.OK);
 
-                String originalName = multipartFile.getOriginalFilename();
-                log.info(multipartFile.getOriginalFilename());
+//        if(upLoadFileDTO.getFiles() != null) {
+//
+//            upLoadFileDTO.getFiles().forEach(multipartFile -> {
+//
+//                String originalName = multipartFile.getOriginalFilename();
+//                log.info(multipartFile.getOriginalFilename());
+//
+//                String uuid = UUID.randomUUID().toString();
+//
+//                Path savePath = Paths.get(upLoadPath, uuid + "_" + originalName);
+//
+//                try {
+//                    multipartFile.transferTo(savePath); //실제 파일 저장함
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });//end each
+//        }
 
-                String uuid = UUID.randomUUID().toString();
-
-                Path savePath = Paths.get(upLoadPath, uuid + "_" + originalName);
-
-                try {
-                    multipartFile.transferTo(savePath); //실제 파일 저장함
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });//end each
-        }
-
-        return "업로드 성공";
     }
 }
--- springBootST/src/main/java/com/boot/s1/dto/upload/UpLoadFileDTO.java
+++ springBootST/src/main/java/com/boot/s1/dto/upload/UpLoadFileDTO.java
@@ -1,13 +1,16 @@
 package com.boot.s1.dto.upload;
 
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Data
+@Builder
 public class UpLoadFileDTO {
 
     private List<MultipartFile> files;
+
+    private String fileName;
 }
