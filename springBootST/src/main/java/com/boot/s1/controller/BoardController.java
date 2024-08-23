40bc98ae1f81ea86b092090a38c7659541f8425d
thegaa632 <thegaa632@gmail.com>
2024-08-23 15:55:31 +0900

    첨부파일 삭제 시 첨부파일 삭제 부분 오류 수정중

--- springBootST/src/main/java/com/boot/s1/controller/BoardController.java
+++ springBootST/src/main/java/com/boot/s1/controller/BoardController.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,12 +16,19 @@ import org.springframework.web.bind.annotation.*;
 import com.boot.s1.service.BoardService;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 @Tag(name = "Controller", description = "API endpoints")
 public class BoardController {
+
+    @Value("${com.boot.s1.upload.path}")
+    private String uploadPath;
 
     private final BoardService boardService;
     @GetMapping("/list")
@@ -103,14 +113,48 @@ public class BoardController {
 //    삭제 파트
     @PostMapping("/remove")
     @Operation(summary = "Post Reamove", description = "remove data")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
+
+        Long bno = boardDTO.getBno();
         log.info("remove post ... " + bno);
 
         boardService.remove(bno);
 
+//      게시물이 데이터베이스상에서 삭제 되었다면 첨부파일을 삭제
+        log.info("파일 이름들 : " + boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+
+        if(fileNames != null && fileNames.size() > 0) {
+            removeFile(fileNames);
+        }
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
+    }
+
+    public void removeFile(List<String> files) {
+
+        for(String fileName:files) {
+            Resource resource = new FileSystemResource(
+                    uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+
+                resource.getFile().delete();
+
+                //섬네일 존재시
+                if(contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+
+                    thumbnailFile.delete();
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+
+        } //end for
     }
 }
