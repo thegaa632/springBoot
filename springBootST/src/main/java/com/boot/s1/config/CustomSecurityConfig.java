ac8a2417ffa5e74f6a9ed0b2029240c3e5dfde2e
thegaa632 <thegaa632@gmail.com>
2024-08-26 16:01:55 +0900

    스프링 시큐리티 적용중

--- springBootST/src/main/java/com/boot/s1/config/CustomSecurityConfig.java
+++ springBootST/src/main/java/com/boot/s1/config/CustomSecurityConfig.java
@@ -3,6 +3,7 @@ package com.boot.s1.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,9 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("----------------------configure----------------------");
+
+        http.formLogin();
 
         return http.build();
     }
---
+++ springBootST/src/main/java/com/boot/s1/security/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.boot.s1.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.info("loadUserByUsername : " + username);
+
+        return null;
+    }
+}
