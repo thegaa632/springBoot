58b9a07aa9d8097794349f8ae2c9fbb6ab197eed
thegaa632 <thegaa632@gmail.com>
2024-09-02 22:01:11 +0900

    스프링 시큐리티 적용중// 하드오류 수정함

--- springBootST/src/main/java/com/boot/s1/controller/MemberController.java
+++ springBootST/src/main/java/com/boot/s1/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.boot.s1.dto.MemberJoinDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
